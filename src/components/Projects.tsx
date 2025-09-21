930c9d7410d91043ae6263157ee03e58437f76e4
SebaConde <sebaconde7@hotmail.es>
2025-09-21 15:07:50 -0300

    Agregue mas proyectos.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -61,6 +61,60 @@ export function ProjectsComponent() {
           </CardFooter>
         </Card>
       </BackgroundGradient> 
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-2 sm:p-4 bg-white dark:bg-zinc-900">
+        <Card className="m-0 border-0 w-80" >
+          <CardHeader>
+          <img
+          src="/StamfordBridge.jpg"
+          alt="jordans"
+          height="70"
+          width="199"
+          className="object-contain"
+        />
+            <CardTitle>Nombre del proyecto</CardTitle>
+            <CardDescription>Descripcion del proyecto</CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <p>stack y links</p>
+          </CardFooter>
+        </Card>
+      </BackgroundGradient> 
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-2 sm:p-4 bg-white dark:bg-zinc-900">
+        <Card className="m-0 border-0 w-80" >
+          <CardHeader>
+          <img
+          src="/StamfordBridge.jpg"
+          alt="jordans"
+          height="70"
+          width="199"
+          className="object-contain"
+        />
+            <CardTitle>Nombre del proyecto</CardTitle>
+            <CardDescription>Descripcion del proyecto</CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <p>stack y links</p>
+          </CardFooter>
+        </Card>
+      </BackgroundGradient> 
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-2 sm:p-4 bg-white dark:bg-zinc-900">
+        <Card className="m-0 border-0 w-80" >
+          <CardHeader>
+          <img
+          src="/StamfordBridge.jpg"
+          alt="jordans"
+          height="70"
+          width="199"
+          className="object-contain"
+        />
+            <CardTitle>Nombre del proyecto</CardTitle>
+            <CardDescription>Descripcion del proyecto</CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <p>stack y links</p>
+          </CardFooter>
+        </Card>
+      </BackgroundGradient> 
       
       
     </div>
